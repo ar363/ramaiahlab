b22ef572bdf6156ad220bad5de12126d10ca9bbf
Aditya Raj <aditya.rajb@gmail.com>
2023-10-27 09:28:49 +0530

    implicit explicit conversion

---
+++ expimpconv.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+  int a, b;
+  double cimp, cexp;
+  printf("Enter 2 numbers\n");
+  scanf("%d%d", &a, &b);
+  
+  
+  cimp = a - b; // implicit conversion
+  cexp = (double)a - b; // explicit conversion
+
+  printf("Implicit result = %.2lf and explicit result = %.2lf\n", cimp, cexp);
+  
+}
