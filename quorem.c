7746ff8c7900458c8ab9510229a28d3ea312c66b
Aditya Raj <49993491+ar363@users.noreply.github.com>
2023-11-02 19:36:42 +0530

    prevent div by 0

--- quorem.c
+++ quorem.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main() {
   int a, b, quotient, remainder;
   
   printf("Enter two numbers \n");
   scanf("%d%d", &a, &b);
+
+  if (b == 0) {
+    printf("Cant divide by zero");
+    exit(0);
+  }
   
   quotient = a/b;
   remainder = a%b;
