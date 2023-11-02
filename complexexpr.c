da2350b0899a4a5ef158093bc19934747008bb36
Aditya Raj <aditya.rajb@gmail.com>
2023-11-02 22:25:59 +0530

    typo

--- complexexpr.c
+++ complexexpr.c
@@ -13,7 +13,7 @@ int main(void)
     y = -a * (3 + b) / 2 - c++ * b;
 
     printf("Value of a * 4 + b / 2 - c * b  is  = %d \n", x);
-    printf("Value of — a * ( 3 + b ) / 2 - c++ * b  is  = %d \n\n", x);
+    printf("Value of — a * ( 3 + b ) / 2 - c++ * b  is  = %d \n\n", y);
 
     printf("Values of the variables after evaluating complex expression: \n");
     printf(" a = %d , b = %d , c = %d \n ", a, b, c);
@@ -27,7 +27,7 @@ int main(void)
 Initial values of the variables:
  a = 3 , b = 4 , c = 5
 Value of a * 4 + b / 2 - c * b  is  = -6
-Value of — a * ( 3 + b ) / 2 - c++ * b  is  = -6
+Value of — a * ( 3 + b ) / 2 - c++ * b  is  = -30
 
 Values of the variables after evaluating complex expression:
  a = 3 , b = 4 , c = 6
