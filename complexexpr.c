1aa18b41a8cbe6c15f939473868c800bf7c64786
Aditya Raj <aditya.rajb@gmail.com>
2023-11-02 20:05:57 +0530

    more presentable?

---
+++ complexexpr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Local Declarations
+    int a = 3;
+    int b = 4;
+    int c = 5;
+    int x;
+    int y;
+    // Statements
+    printf("Initial values of the variables: \n");
+    printf(" a = %d , b = %d , c = %d \n", a, b, c);
+
+    x = a * 4 + b / 2 - c * b;
+    y = -a * (3 + b) / 2 - c++ * b;
+
+    printf("Value of a * 4 + b / 2 - c * b  is  = %d \n", x);
+    printf("Value of — a * ( 3 + b ) / 2 - c++ * b  is  = %d \n\n", x);
+
+    printf("Values of the variables after evaluating complex expression: \n");
+    printf(" a = %d , b = %d , c = %d \n ", a, b, c);
+
+    return 0;
+}
+
+/*
+ OUTPUT ::
+
+Initial values of the variables:
+ a = 3 , b = 4 , c = 5
+Value of a * 4 + b / 2 - c * b  is  = -6
+Value of — a * ( 3 + b ) / 2 - c++ * b  is  = -6
+
+Values of the variables after evaluating complex expression:
+ a = 3 , b = 4 , c = 6
+
+*/
--- sales.c
+++ sales.c
@@ -1,20 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-  float unitprice, qty, disc, taxRate, subtotal, grandtotal, taxamt;
-  
+int main()
+{
+  float unitprice, qty, disc, taxRate, subtotal, subtaxable, grandtotal, taxamt;
+
   printf("Enter unit price, qty, discount and tax rate\n");
   scanf("%f%f%f%f", &unitprice, &qty, &disc, &taxRate);
-  
-  subtotal = (unitprice * qty) - disc;
-  taxamt = subtotal * taxRate / 100;
-  grandtotal = subtotal + taxamt;
-  
-  
+
+  subtotal = (unitprice * qty);
+  subtaxable = subtotal - disc;
+  taxamt = subtaxable * taxRate / 100;
+  grandtotal = subtaxable + taxamt;
+
+  printf("Unit Price: %.2f\n", unitprice);
+  printf("Subtotal: %.2f\n", subtotal);
+  printf("Discount: -%.2f\n", disc);
+  printf("Discounted Total: %.2f\n", subtaxable);
+  printf("Sales Tax: %.2f\n\n", taxamt);
+
   printf("Total sales is %f \n", grandtotal);
-  
 }
 
+/*
 
+OUTPUT :: 
 
+Enter unit price, qty, discount and tax rate
+50 200 250 18
+Unit Price: 50.00
+Subtotal: 10000.00
+Discount: -250.00
+Discounted Total: 9750.00
+Sales Tax: 1755.00
 
+Total sales is 11505.000000
+
+*/
