1a5293f9e9b81789adb1fb1eb0e8208f59daaadd
Aditya Raj <aditya.rajb@gmail.com>
2023-11-17 10:02:59 +0530

    add sum of dig

---
+++ sumofdig.c
@@ -0,0 +1,24 @@
+/*
+sum of digits 
+*/
+
+#include<stdio.h>
+
+void main() {
+	int x, sumofdigs = 0, noofdigs = 0, i = 0;
+	printf("Enter number: ");
+	scanf("%d", &x);
+	printf("Your number is %d\n", x);
+	
+	while (x != 0) {
+		noofdigs++;
+		sumofdigs += x%10;
+		x /= 10;
+	}
+	
+	printf("The number of digits is %d\n", noofdigs);
+	printf("The sum of digits is %d\n", sumofdigs);
+	
+	
+}
+
