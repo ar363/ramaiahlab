8c2bd632131888774e2672f8093602a7434b7c3b
Aditya Raj <aditya.rajb@gmail.com>
2023-11-24 10:41:25 +0530

    matrices

--- bubblesort.c
+++ bubblesort.c
@@ -10,9 +10,23 @@ printf("Enter the elements:\n");
 for(i=0; i<n; i++) scanf("%d", &a[i]);
 
 for(i=0; i<n-1; i++) {
-  for(j=0; j<n-2; j++) {
-    
+  for(j=0; j<n-i-1; j++) {
+    if(a[j] > a[j+1]) {
+    	int temp = a[j];
+    	a[j]=a[j+1];
+    	a[j+1]=temp;
+    }
   }
 }
 
+
+for(i=0; i<n; i++) printf("%d ", a[i]);
+
 }
+
+/*
+Enter the size of array: 5 
+Enter the elements:
+12 54 10 9 23
+9 10 12 23 54
+*/
