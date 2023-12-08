f369985bde4e222dd65df485b77e241ff057ba75
Aditya Raj <aditya.rajb@gmail.com>
2023-12-08 09:23:15 +0530

    transpose

---
+++ transposemat.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+    int a[10][10], at[10][10], n, i, j, k;
+
+    printf("Enter the no of elements in matrix ");
+    scanf("%d", &n);
+    printf("Enter the elements of matrix: \n");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            at[i][j] = a[j][i];
+        }
+    }
+
+    printf("The transpose is: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d\t", at[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
