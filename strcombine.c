6580c1a0018f27156cea78bce91b57e4a2c790aa
Aditya Raj <aditya.rajb@gmail.com>
2023-12-15 09:52:02 +0530

    string combine

---
+++ strcombine.c
@@ -0,0 +1,32 @@
+// not working
+
+#include<stdio.h>
+
+void main() {
+	char s1[100], s2[100], sComb[200];
+	int i = 0, s1Length = 0, s2Length;
+
+	printf("Enter a string\n");
+	scanf("%s", s1);
+	
+	printf("Enter another string\n");
+	scanf("%s", s2);
+	
+	
+	while(s1[i] != '\0') {
+		sComb[i] = s1[i];
+		s1Length++;
+		i++;
+	}
+	
+	i = 0;
+	while(s2[i] != '\0') {
+		sComb[s1Length + i]  = s2[i];
+		s2Length++;
+		i++;
+	}
+	
+	sComb[s1Length + s2Length] = '\0';
+	
+	printf("THe combined string is\n%s\n", sComb);
+}
