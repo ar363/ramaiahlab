99fcb8092b3f1de7d759ade16049c06a29db183f
Aditya Raj <aditya.rajb@gmail.com>
2023-12-15 09:28:16 +0530

    fmt

--- hcflcm.c
+++ hcflcm.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 
 void calcHCFandLCM(int a, int b) {
-		int hcf, lcm;
+	int hcf, lcm;
 		
-		for(int i = 1; i<=a && i<=b; i++) {
-        if(a%i == 0 && b%i == 0) hcf = i;
-    }
+	for(int i = 1; i<=a && i<=b; i++) {
+		if(a%i == 0 && b%i == 0) hcf = i;
+	}
 
-    lcm = (a*b) / hcf;
+	lcm = (a*b) / hcf;
     
-    printf("HCF is %d and LCM is %d\n", hcf, lcm);
+	printf("HCF is %d and LCM is %d\n", hcf, lcm);
 }
 
 
 void main() {
-    int a, b;
-    printf("Enter the 2 numbers: ");
-    scanf("%d%d", &a, &b);
+	int a, b;
+	printf("Enter the 2 numbers: ");
+	scanf("%d%d", &a, &b);
 
-    calcHCFandLCM(a, b);
+	calcHCFandLCM(a, b);
 }
 
 
