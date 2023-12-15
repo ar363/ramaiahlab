faeffdcabac0b79ea7ad321b84094295f135b89a
Aditya Raj <aditya.rajb@gmail.com>
2023-12-15 10:12:21 +0530

    str compare proper

--- strcompare.c
+++ strcompare.c
@@ -4,7 +4,7 @@
 
 void main() {
 	char s1[100], s2[100];
-	int s1Length = 0, s2Length = 0;
+	int s1Length = 0, s2Length = 0, i = 0;
 
 	printf("Enter a string\n");
 	scanf("%s", s1);
@@ -14,11 +14,13 @@ void main() {
 	
 	while(s1[s1Length] != '\0') s1Length++;
 	
-	while(s2[s2Length] != '\0') s2Length++; 
+	while(s2[s2Length] != '\0') s2Length++;
 	
-	if(s1Length > s2Length) 
+	while(s1[i] == s2[i] && s1[i] != '\0') i++;
+	
+	if(s1[i] > s2[i]) 
 		printf("First string is greater\n");
-	else if (s1Length < s2Length)
+	else if (s1[i] < s2[i])
 		printf("Second string is greater\n");
 	else
 		printf("Strings are equal\n");
