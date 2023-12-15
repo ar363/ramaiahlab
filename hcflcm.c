21e1ba087f9b2075c5bea8db45afea0b647b7119
Aditya Raj <aditya.rajb@gmail.com>
2023-12-15 09:25:59 +0530

    use fns

--- hcflcm.c
+++ hcflcm.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
-void main() {
-    int a, b, lcm, hcf;
-    printf("Enter the 2 numbers: ");
-    scanf("%d%d", &a, &b);
 
-    for(int i = 1; i<=a && i<=b; i++) {
+void calcHCFandLCM(int a, int b) {
+		int hcf, lcm;
+		
+		for(int i = 1; i<=a && i<=b; i++) {
         if(a%i == 0 && b%i == 0) hcf = i;
     }
 
     lcm = (a*b) / hcf;
-
-    printf("HCF is %d and LCM is %d", hcf, lcm);
+    
+    printf("HCF is %d and LCM is %d\n", hcf, lcm);
 }
+
+
+void main() {
+    int a, b;
+    printf("Enter the 2 numbers: ");
+    scanf("%d%d", &a, &b);
+
+    calcHCFandLCM(a, b);
+}
+
 
